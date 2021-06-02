f70c65062bfc110fca41e6b5bfca62c5810d82bd
MateuszKost <matekos1604@gmail.com>
2021-06-02 23:32:26 +0200

    contact's and skill's view finished

--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ namespace MateuszKostCV
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddScoped<ClipboardService>();
+
             await builder.Build().RunAsync();
         }
     }
